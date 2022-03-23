25c7776c771f03024b5c9e9e48b8e895628a2e70
youyou47000 <youssef.bouamma@hotmail.fr>
2022-03-23 15:03:53 +0100

    modif commentaire

--- src/UPJV/Validator/EstValide.php
+++ src/UPJV/Validator/EstValide.php
@@ -1,15 +1,17 @@
 <?php
-
+/*
+ * This file is part of the Symfony package.
+ */
 namespace UPJV\Validator;
 
-class EstValide implements ValidatorInterface
+class EstValide implements ValidatorInterface // class
 {
-    public function build(array $param): object
+    public function build(array $param): object //bluid
     {
         return $this;
     }
 
-    public function check($input): bool
+    public function check($input): bool //check
     {
         return true;
     }
